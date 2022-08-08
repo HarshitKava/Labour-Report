39c76955b98f28dee396a90c70532e0056a427e8
HarshitKava <harshit.kava@gmail.com>
2022-08-08 13:37:42 +0530

    5

--- Area_1/Area_1.js
+++ Area_1/Area_1.js
@@ -3,3 +3,8 @@ var y = n.getFullYear();
 var m = n.getMonth() + 1;
 var d = n.getDate();
 document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+
+function checkpass(){ 
+    location.replace("../index.html");
+};
+document.querySelector(".bkbtn").addEventListener("click",checkpass);
