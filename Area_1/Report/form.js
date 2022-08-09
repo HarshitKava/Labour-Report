cd6ceb84c320fb08e6b43627a7c4d83b78a17511
HarshitKava <harshit.kava@gmail.com>
2022-08-09 18:43:02 +0530

    1

--- Area_1/Report/form.js
+++ Area_1/Report/form.js
@@ -8,3 +8,22 @@ function checkpass(){
     location.replace("../Area_1.html");
 };
 document.querySelector(".bkbtn").addEventListener("click",checkpass);
+
+
+var mycars = ['Herr','Frau'];
+var list = document.getElementById('Name');
+
+mycars.forEach(function(item){
+   var option = document.createElement('option');
+   option.value = item;
+   list.appendChild(option);
+});
+
+
+var n= document.getElementById('name');
+n.addEventListener("input",()=>{
+    if (n.value!=""){
+        document.querySelector(".add").classList.toggle("visible");
+    }
+    console.log("hello")
+});
