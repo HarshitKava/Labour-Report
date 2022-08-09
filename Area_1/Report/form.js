a577e9fef6b7e15b970f94df7f1b60c5afa4aa6a
HarshitKava <harshit.kava@gmail.com>
2022-08-09 21:49:16 +0530

    1

--- Area_1/Report/form.js
+++ Area_1/Report/form.js
@@ -4,26 +4,65 @@ var m = n.getMonth() + 1;
 var d = n.getDate();
 document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
 
-function checkpass(){ 
+function goBack(){ 
     location.replace("../Area_1.html");
 };
-document.querySelector(".bkbtn").addEventListener("click",checkpass);
+document.querySelector(".bkbtn").addEventListener("click",goBack);
+
+function chooselabour(x){
+    if (x=="type-1"){
+        var type1_labour = ['MAS','CAR'];
+        var select = document.getElementById('labour');
+
+        select.innerHTML="";
+        var option = document.createElement("OPTION"),
+            txt =document.createTextNode( 'None');
+        option.appendChild(txt);
+        select.insertBefore(option,select.lastChild);
 
 
-var mycars = ['Herr','Frau'];
-var list = document.getElementById('Name');
-
-mycars.forEach(function(item){
-   var option = document.createElement('option');
-   option.value = item;
-   list.appendChild(option);
-});
-
-
-var n= document.getElementById('name');
-n.addEventListener("input",()=>{
-    if (n.value!=""){
-        document.querySelector(".add").classList.toggle("visible");
+        for (var i = 0; i < type1_labour.length; i++) {
+            var option = document.createElement("OPTION"),
+                txt =document.createTextNode( type1_labour[i]);
+            option.appendChild(txt);
+            select.insertBefore(option,select.lastChild);
+        }
     }
-    console.log("hello")
-});
+    else if (x=="type-2"){
+        var type2_labour = ['FAB','CAR'];
+        var select = document.getElementById('labour');
+
+        select.innerHTML="";
+        var option = document.createElement("OPTION"),
+            txt =document.createTextNode( 'None');
+        option.appendChild(txt);
+        select.insertBefore(option,select.lastChild);
+
+
+        for (var i = 0; i < type2_labour.length; i++) {
+            var option = document.createElement("OPTION"),
+                txt =document.createTextNode( type2_labour[i]);
+            option.appendChild(txt);
+            select.insertBefore(option,select.lastChild);
+        }
+    
+    }
+    else{
+        var select = document.getElementById('labour');
+
+        select.innerHTML="";
+        var option = document.createElement("OPTION"),
+            txt =document.createTextNode( 'None');
+        option.appendChild(txt);
+        select.insertBefore(option,select.lastChild);
+    }
+}
+
+function categoryOfLabour(x){
+    if (x=='None'){
+        document.querySelector(".typeOfLabour").classList.toggle("invisible");
+    }
+    else{
+        document.querySelector(".typeOfLabour").classList.toggle("visible");
+    }
+}
