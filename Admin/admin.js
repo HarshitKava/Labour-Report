24d97c23cf1e1e341f353576f064b64fdfebd035
HarshitKava <harshit.kava@gmail.com>
2022-08-10 11:51:37 +0530

    1

--- Admin/admin.js
+++ Admin/admin.js
@@ -3,3 +3,8 @@ var y = n.getFullYear();
 var m = n.getMonth() + 1;
 var d = n.getDate();
 document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
+
+function goBack(){ 
+    location.replace("../index.html");
+};
+document.querySelector(".bkbtn").addEventListener("click",goBack);
