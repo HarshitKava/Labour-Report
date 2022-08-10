1cd2a71225273a33c7f3d8853a89753d5e719d5c
HarshitKava <harshit.kava@gmail.com>
2022-08-10 13:30:20 +0530

    1

--- Admin/admin.js
+++ Admin/admin.js
@@ -8,3 +8,8 @@ function goBack(){
     location.replace("../index.html");
 };
 document.querySelector(".bkbtn").addEventListener("click",goBack);
+
+function addSiteEng() {
+    location.replace("SiteEng/SiteEng.html");
+}
+document.querySelector(".addSE").addEventListener("click",addSiteEng)
